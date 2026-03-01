547c0e84d38e28f55d5f308e0257a3c9c7496655
Inna Borodina <creator.geek@gmail.com>
2026-03-01 20:27:15 +0300

    status code fixed

--- internal/handler/getbyidhandler.go
+++ internal/handler/getbyidhandler.go
@@ -6,23 +6,23 @@ import (
 
 func (h *UrlHandler) GetByIdGet(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
-		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
-		return
+		http.Error(res, "Only GET requests are allowed!", http.StatusBadRequest)
 	}
 
 	id := req.PathValue("id")
 
 	if id == "" {
 		http.Error(res, "ID not provided", http.StatusBadRequest)
-		return
 	}
 
 	result, err := h.urlService.GetById(id)
 
 	if err != nil {
-		http.Error(res, "URL not found", http.StatusNotFound)
-		return
+		http.Error(res, "URL not found", http.StatusBadRequest)
 	}
 
-	http.Redirect(res, req, result, http.StatusTemporaryRedirect)
+	//http.Redirect(res, req, result, http.StatusTemporaryRedirect)
+
+	res.Header().Set("Location", result)
+	res.WriteHeader(http.StatusTemporaryRedirect)
 }
