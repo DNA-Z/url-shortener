8069d3b8d3b677f2f14ed331dee6b1748fa65835
Inna Borodina <creator.geek@gmail.com>
2026-03-06 22:38:06 +0300

    return error

--- internal/handler/getbyidhandler.go
+++ internal/handler/getbyidhandler.go
@@ -7,22 +7,22 @@ import (
 func (h *URLHandler) GetByIDGet(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
 		http.Error(res, "Only GET requests are allowed!", http.StatusBadRequest)
+		return
 	}
 
 	id := req.PathValue("id")
 
 	if id == "" {
 		http.Error(res, "ID not provided", http.StatusBadRequest)
+		return
 	}
 
 	result, err := h.urlService.GetByID(id)
 
 	if err != nil {
 		http.Error(res, "URL not found", http.StatusBadRequest)
+		return
 	}
 
-	//http.Redirect(res, req, result, http.StatusTemporaryRedirect)
-
-	res.Header().Set("Location", result)
-	res.WriteHeader(http.StatusTemporaryRedirect)
+	http.Redirect(res, req, result, http.StatusTemporaryRedirect)
 }
