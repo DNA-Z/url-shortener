ba57cdf43eb59c98225c33e17c6d154229ca1a7c
Inna Borodina <creator.geek@gmail.com>
2026-05-01 23:56:50 +0300

    db config fixed

--- internal/config/options.go
+++ internal/config/options.go
@@ -17,7 +17,7 @@ func NewOptions() *Options {
 		ServerAddress:    "localhost:8080",
 		BaseURL:          "http://localhost:8080/",
 		FileStoragePath:  "short_url",
-		ConnectionString: "host=localhost port=5432 user=postgres password=lightning dbname=short_url sslmode=disable", //"postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable",
+		ConnectionString: "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable",
 	}
 }
 
