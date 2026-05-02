9dde805fbd727f4cd765cd5862ff815f6fe932cb
Inna Borodina <creator.geek@gmail.com>
2026-05-02 15:01:18 +0300

    db connection refactoring

--- internal/handler/dbpinghandler.go
+++ internal/handler/dbpinghandler.go
@@ -3,21 +3,27 @@ package handler
 import (
 	"context"
 	"database/sql"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/DNA-Z/url-shortener/internal/config"
+	"github.com/DNA-Z/url-shortener/internal/storage"
 )
 
+var defaultConnnectionStr string = "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable"
+
 type DBPingHandler struct {
-	db            *sql.DB
-	serverAddress string
-	baseURL       string
+	dbConnectionString string
+	serverAddress      string
+	baseURL            string
 }
 
-func NewDBPingHandler(db *sql.DB, serverAddress string, baseURL string) *DBPingHandler {
+func NewDBPingHandler(cfg *config.Options) *DBPingHandler {
 	return &DBPingHandler{
-		db:            db,
-		serverAddress: serverAddress,
-		baseURL:       baseURL,
+		dbConnectionString: cfg.ConnectionString,
+		serverAddress:      cfg.ServerAddress,
+		baseURL:            cfg.BaseURL,
 	}
 }
 
@@ -30,10 +36,24 @@ func (h *DBPingHandler) GetDbPing(res http.ResponseWriter, req *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err := h.db.PingContext(ctx); err != nil {
+	db := getDB(h.dbConnectionString)
+	if err := db.PingContext(ctx); err != nil {
 		http.Error(res, "Database connection failed", http.StatusInternalServerError)
 		return
 	}
 
 	res.WriteHeader(http.StatusOK)
+}
+
+func getDB(connectionString string) *sql.DB {
+	if connectionString == "" {
+		connectionString = defaultConnnectionStr
+	}
+	ctx := context.Background()
+	database, err := storage.DbConnect(ctx, connectionString)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	return database
 }
