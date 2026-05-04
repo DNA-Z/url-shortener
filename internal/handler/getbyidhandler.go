4c1d661a4c256510cea02cbaecbfc58d7d131a63
Inna Borodina <creator.geek@gmail.com>
2026-05-04 17:00:44 +0300

    GetByIDGet rollback

--- internal/handler/getbyidhandler.go
+++ internal/handler/getbyidhandler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"log"
 	"net/http"
-	"strings"
 )
 
 func (h *URLHandler) GetByIDGet(res http.ResponseWriter, req *http.Request) {
@@ -14,10 +13,6 @@ func (h *URLHandler) GetByIDGet(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	normalizeRequestURL(req)
-	log.Printf("Normalized URL: scheme=%s host=%s path=%s",
-		req.URL.Scheme, req.URL.Host, req.URL.Path)
-
 	id := req.PathValue("id")
 	log.Printf("found short URL: %v", id)
 
@@ -27,30 +22,11 @@ func (h *URLHandler) GetByIDGet(res http.ResponseWriter, req *http.Request) {
 	}
 
 	result, err := h.urlService.GetByID(id)
-
 	if err != nil {
-		http.Error(res, "URL not found", http.StatusBadRequest)
+		http.Error(res, "URL not found", http.StatusNotFound)
 		return
 	}
 
 	res.Header().Set("Location", result)
 	res.WriteHeader(http.StatusTemporaryRedirect)
-}
-
-func normalizeRequestURL(req *http.Request) {
-	if req.URL.Scheme == "" {
-		req.URL.Scheme = "http"
-	}
-
-	if req.URL.Host == "" {
-		req.URL.Host = "localhost:8080"
-	}
-
-	if req.URL.Path == "" || !strings.HasPrefix(req.URL.Path, "/") {
-		if req.URL.Path != "" {
-			req.URL.Path = "/" + req.URL.Path
-		} else {
-			req.URL.Path = "/"
-		}
-	}
 }
