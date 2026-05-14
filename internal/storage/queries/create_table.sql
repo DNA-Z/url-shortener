a9124db357466bd71a6984b7921566e54376a84e
Inna Borodina <creator.geek@gmail.com>
2026-05-14 11:58:51 +0300

    query fix

--- internal/storage/queries/create_table.sql
+++ internal/storage/queries/create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS urls (
     uuid UUID PRIMARY KEY,
     user_id uuid NULL,
-    short_url TEXT NULL,
+    short_url VARCHAR(255) NULL,
     original_url TEXT NULL,
     id_deleted BOOLEAN DEFAULT FALSE
 );
