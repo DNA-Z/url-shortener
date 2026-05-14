5ad590dbb5538e42f09d57751672fc84f2c09869
Inna Borodina <creator.geek@gmail.com>
2026-05-14 10:39:53 +0300

    migration changed

--- migrations/000001_create_shortened_urls_table.down.sql
+++ migrations/000001_create_shortened_urls_table.down.sql
@@ -1,4 +1,5 @@
 -- migrations/000001_create_shortened_urls_table.down.sql
 DROP INDEX IF EXISTS idx_original_url;
 DROP INDEX IF EXISTS idx_short_url;
+DROP INDEX IF EXISTS idx_user_id;
 DROP TABLE IF EXISTS urls;
--- migrations/000001_create_shortened_urls_table.up.sql
+++ migrations/000001_create_shortened_urls_table.up.sql
@@ -1,9 +1,12 @@
 -- migrations/000001_create_shortened_urls_table.up.sql
 CREATE TABLE urls (
                         id uuid PRIMARY KEY,
-                        short_url VARCHAR(255) NOT NULL,
-                        original_url TEXT NOT NULL
+                        user_id uuid NULL,
+                        short_url VARCHAR(255) NULL,
+                        original_url TEXT NULL,
+                        id_deleted BOOLEAN DEFAULT FALSE
 );
 
+CREATE INDEX idx_user_id ON urls(user_id);
 CREATE INDEX idx_short_url ON urls(short_url);
 CREATE INDEX idx_original_url ON urls(original_url);
