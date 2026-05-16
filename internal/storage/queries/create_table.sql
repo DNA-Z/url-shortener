d1d57eaf27541f2ff23cbac4e4cdfcef22ce99d5
Inna Borodina <creator.geek@gmail.com>
2026-05-17 02:04:07 +0300

    sql fix

--- internal/storage/queries/create_table.sql
+++ internal/storage/queries/create_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS urls (
     uuid UUID PRIMARY KEY,
     user_id UUID NULL,
     short_url VARCHAR(255) NULL,
-    original_url TEXT NULL
+    original_url TEXT NULL,
     is_deleted BOOLEAN DEFAULT FALSE
 );
--- migrations/000001_create_shortened_urls_table.up.sql
+++ migrations/000001_create_shortened_urls_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE urls (
                         id uuid PRIMARY KEY,
                         user_id UUID NULL,
                         short_url VARCHAR(255) NULL,
-                        original_url TEXT NULL
+                        original_url TEXT NULL,
                         is_deleted BOOLEAN DEFAULT FALSE
 );
 
