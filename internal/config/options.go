af328e712249b60e2400445467987275895a1697
Inna Borodina <creator.geek@gmail.com>
2026-08-04 13:47:09 +0300

    config file added

--- internal/config/options.go
+++ internal/config/options.go
@@ -9,9 +9,20 @@
 package config
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"os"
 )
+
+// JSONConfig представляет структуру конфигурационного файла.
+type JSONConfig struct {
+	ServerAddress   string `json:"server_address"`
+	BaseURL         string `json:"base_url"`
+	FileStoragePath string `json:"file_storage_path"`
+	DatabaseDSN     string `json:"database_dsn"`
+	EnableHTTPS     bool   `json:"enable_https"`
+}
 
 // Options содержит все настройки сервиса.
 type Options struct {
@@ -23,6 +34,7 @@ type Options struct {
 	AuditFile        string
 	AuditURL         string
 	EnableHTTPS      bool
+	ConfigFile       string
 }
 
 // NewOptions создает новый экземпляр Options со значениями по умолчанию.
@@ -36,6 +48,7 @@ func NewOptions() *Options {
 		AuditFile:        "",
 		AuditURL:         "",
 		EnableHTTPS:      false,
+		ConfigFile:       "",
 	}
 }
 
@@ -49,6 +62,9 @@ func (o *Options) OptionsInit() {
 	defaultAuditFile := o.AuditFile
 	defaultAuditURL := o.AuditURL
 	defaultEnableHTTPS := o.EnableHTTPS
+	defaultConfigFile := o.ConfigFile
+
+	var jsonConfig *JSONConfig
 
 	if flag.Lookup("a") == nil {
 		serverAddressFlag := flag.String("a", defaultServerAddress, "адрес HTTP-сервера")
@@ -59,63 +75,76 @@ func (o *Options) OptionsInit() {
 		auditFileFlag := flag.String("audit-file", defaultAuditFile, "аудит запросов с записью логов в файл")
 		auditURLFlag := flag.String("audit-url", defaultAuditURL, "URL сервера для отправки логов аудита")
 		enableHTTPSFlag := flag.Bool("s", defaultEnableHTTPS, "включить HTTPS")
+		configFile := flag.String("c", defaultConfigFile, "конфигурационный файл")
 
 		flag.Parse()
 
-		o.ServerAddressSet(serverAddressFlag)
-		o.BaseURLSet(baseURLFlag)
-		o.PathToFile(fileStoragePath)
-		o.ConnectionStringSet(connectionStringFlag)
+		jsonConfig = o.readConfigFile(configFile)
+
+		o.ServerAddressSet(serverAddressFlag, jsonConfig)
+		o.BaseURLSet(baseURLFlag, jsonConfig)
+		o.PathToFile(fileStoragePath, jsonConfig)
+		o.ConnectionStringSet(connectionStringFlag, jsonConfig)
 		o.SecretKeySet(secretKeyFlag)
 		o.AuditFileSet(auditFileFlag)
 		o.AuditURLSet(auditURLFlag)
-		o.EnableHTTPSSet(enableHTTPSFlag)
+		o.EnableHTTPSSet(enableHTTPSFlag, jsonConfig)
+		o.ConfigFileSet(configFile)
 	} else {
 		// Флаги уже проинициализированы
-		o.ServerAddressSet(&o.ServerAddress)
-		o.BaseURLSet(&o.BaseURL)
-		o.PathToFile(&o.FileStoragePath)
-		o.ConnectionStringSet(&o.ConnectionString)
+		o.ServerAddressSet(&o.ServerAddress, jsonConfig)
+		o.BaseURLSet(&o.BaseURL, jsonConfig)
+		o.PathToFile(&o.FileStoragePath, jsonConfig)
+		o.ConnectionStringSet(&o.ConnectionString, jsonConfig)
 		o.SecretKeySet(&o.SecretKey)
 		o.AuditFileSet(&o.AuditFile)
 		o.AuditURLSet(&o.AuditURL)
-		o.EnableHTTPSSet(&o.EnableHTTPS)
+		o.EnableHTTPSSet(&o.EnableHTTPS, jsonConfig)
+		o.ConfigFileSet(&o.ConfigFile)
 	}
 }
 
-func (o *Options) ServerAddressSet(serverAddressFlag *string) {
+func (o *Options) ServerAddressSet(serverAddressFlag *string, jsonConfig *JSONConfig) {
 	switch {
 	case os.Getenv("SERVER_ADDRESS") != "":
 		o.ServerAddress = os.Getenv("SERVER_ADDRESS")
 	case *serverAddressFlag != o.ServerAddress:
 		o.ServerAddress = *serverAddressFlag
+	case jsonConfig != nil && jsonConfig.ServerAddress != "":
+		o.ServerAddress = jsonConfig.ServerAddress
 	}
 }
 
-func (o *Options) BaseURLSet(baseURLFlag *string) {
+func (o *Options) BaseURLSet(baseURLFlag *string, jsonConfig *JSONConfig) {
 	switch {
 	case os.Getenv("BASE_URL") != "":
 		o.BaseURL = os.Getenv("BASE_URL")
 	case *baseURLFlag != o.BaseURL:
 		o.BaseURL = *baseURLFlag
+	case jsonConfig != nil && jsonConfig.BaseURL != "":
+		o.BaseURL = jsonConfig.BaseURL
 	}
 }
 
-func (o *Options) PathToFile(fileStoragePath *string) {
+func (o *Options) PathToFile(fileStoragePath *string, jsonConfig *JSONConfig) {
 	switch {
 	case os.Getenv("FILE_STORAGE_PATH") != "":
 		o.FileStoragePath = os.Getenv("FILE_STORAGE_PATH")
 	case *fileStoragePath != o.FileStoragePath:
 		o.FileStoragePath = *fileStoragePath
+	case jsonConfig != nil && jsonConfig.FileStoragePath != "":
+		o.FileStoragePath = jsonConfig.FileStoragePath
 	}
 }
 
-func (o *Options) ConnectionStringSet(connectionStringFlag *string) {
+func (o *Options) ConnectionStringSet(connectionStringFlag *string, jsonConfig *JSONConfig) {
 	switch {
 	case os.Getenv("DATABASE_DSN") != "":
 		o.ConnectionString = os.Getenv("DATABASE_DSN")
 	case *connectionStringFlag != o.ConnectionString:
 		o.ConnectionString = *connectionStringFlag
+	case jsonConfig != nil && jsonConfig.DatabaseDSN != "":
+		o.ConnectionString = jsonConfig.DatabaseDSN
 	}
 }
 
@@ -146,11 +175,53 @@ func (o *Options) AuditURLSet(auditURLFlag *string) {
 	}
 }
 
-func (o *Options) EnableHTTPSSet(enableHTTPSFlag *bool) {
+func (o *Options) EnableHTTPSSet(enableHTTPSFlag *bool, jsonConfig *JSONConfig) {
 	switch {
 	case os.Getenv("ENABLE_HTTPS") != "":
 		o.EnableHTTPS = os.Getenv("ENABLE_HTTPS") == "true"
 	case *enableHTTPSFlag != o.EnableHTTPS:
 		o.EnableHTTPS = *enableHTTPSFlag
+	case jsonConfig != nil && jsonConfig.EnableHTTPS == true:
+		o.EnableHTTPS = jsonConfig.EnableHTTPS
 	}
+}
+
+func (o *Options) ConfigFileSet(configFileFlag *string) {
+	switch {
+	case os.Getenv("CONFIG") != "":
+		o.ConfigFile = os.Getenv("CONFIG")
+	case *configFileFlag != o.ConfigFile:
+		o.ConfigFile = *configFileFlag
+	}
+}
+
+func (o *Options) readConfigFile(configFileFlag *string) *JSONConfig {
+	configFile := *configFileFlag
+	if configFile == "" {
+		configFile = os.Getenv("CONFIG")
+	}
+	if configFile == "" {
+		return nil
+	}
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(file)
+	if err != nil {
+		return nil
+	}
+
+	data := buf.Bytes()
+
+	var cfg JSONConfig
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil
+	}
+
+	return &cfg
 }
